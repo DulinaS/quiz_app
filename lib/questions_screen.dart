941f8ac6396623c90506202e08ae000953fc9ec8
Dulina Senarathne <dulinahansa2002@gmail.com>
2024-11-06 23:20:24 +0530

    Accessing list items in Question list

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/quiz.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,18 +15,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    //Get the current question out of questions list
+    final currentQuestion = questions[0];
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        const Text(
-          'The Question....',
-          style: TextStyle(color: Colors.white),
+        Text(
+          currentQuestion.text,
+          style: const TextStyle(color: Colors.white),
         ),
         const SizedBox(height: 30),
-        AnswerButton(answerText: 'Answer 1', onTap: () {}),
-        AnswerButton(answerText: 'Answer 2', onTap: () {}),
-        AnswerButton(answerText: 'Answer 3', onTap: () {}),
-        AnswerButton(answerText: 'Answer 4', onTap: () {}),
+        AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+        AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+        AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+        AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
       ],
     );
   }

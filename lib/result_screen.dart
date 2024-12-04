6af3e72298e9c78500c8ca1cc96528ef85eebeca
Dulina Senarathne <dulinahansa2002@gmail.com>
2024-12-05 01:18:15 +0530

    Created a method to create a summary list. Summary list include question,correct answer and user selected answer

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key , required this.chosenAnswers}); //as a named argument
+  const ResultScreen(
+      {super.key, required this.chosenAnswers}); //as a named argument
 
   //Results screen will accept the user selected answers list from gradientContainer
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i, //Adds the question index
+          'question':
+              questions[i].text, //Adds each question one by one to the map
+          'correct_answer': questions[i].answers[0], //we assume first answer in questions list is the correct answer
+          'user_answer': chosenAnswers[i] //we collect user selected answers because we display the questions from start
+                                          // So we load the display from questions list from start answer won;t get mixed
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +33,17 @@ class ResultScreen extends StatelessWidget {
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
-        child:  Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('You answers X out of Y questions correct'),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             const Text('list of answers and questions'),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             TextButton(onPressed: () {}, child: const Text('Restart Quiz!'))
           ],
         ),

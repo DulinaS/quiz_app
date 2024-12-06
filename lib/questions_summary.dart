5d40bf6bc59d83b70696ea1ce3970eea6903bf92
Dulina Senarathne <dulinahansa2002@gmail.com>
2024-12-06 12:43:45 +0530

    Added a feature to make the result summary scrollable using the SingleChildScrollView widget

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -11,26 +11,28 @@ class QuestionsSummary extends StatelessWidget {
     //This returns a column. In the column each row inside the column represent one piece of infomation from summaryDats
     return SizedBox(
       height: 300,
-      child: Column(
-        children: summaryData.map(
-          (data) {
-            return Row(
-              children: [
-                Text(((data['question_index'] as int) + 1).toString()),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(data['question'] as String),
-                      const SizedBox(height: 5,),
-                      Text(data['user_answer'] as String),
-                      Text(data['correct_answer'] as String),
-                    ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return Row(
+                children: [
+                  Text(((data['question_index'] as int) + 1).toString()),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(data['question'] as String),
+                        const SizedBox(height: 5,),
+                        Text(data['user_answer'] as String),
+                        Text(data['correct_answer'] as String),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            );
-          },
-        ).toList(),
+                ],
+              );
+            },
+          ).toList(),
+        ),
       ),
     );
   }
